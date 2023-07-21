6b8c958b4fb2b0a0708f3a4f98307de972f4cddb
ali morgan <alimorgaan@gmail.com>
2023-07-21 14:47:09 +0300

    vercel backend

--- backend/dist/routers/hello.js
+++
@@ -1,7 +0,0 @@
-import { router } from "../trpc";
-import { publicProcedure } from "../trpc";
-export const helloRouter = router({
-    sayHello: publicProcedure.query(() => {
-        return "Hello World";
-    })
-});
--- backend/dist/routers/index.js
+++
@@ -1,9 +0,0 @@
-import { router } from "../trpc";
-import { helloRouter } from "./hello";
-import { noteRouter } from "./note";
-import { userRouter } from "./user";
-export const appRouter = router({
-    hello: helloRouter,
-    user: userRouter,
-    note: noteRouter
-});
